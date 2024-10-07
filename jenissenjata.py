c9dda14e2020753018dd71943cf95ac26ab404ad
aulianurfauziah <auliaaafauziah99@gmail.com>
2024-10-07 15:18:57 +0700

    Menghapus file yang tidak diperlukan

--- jenissenjata.py
+++
@@ -1,9 +0,0 @@
-# Fungsi untuk membaca file jenis_senjata (data1.txt)
-def baca_jenis_senjata(filename):
-    jenis_senjata = {}
-    with open(filename, 'r') as file:
-        for line in file:
-            print(f"Reading line: {line}")  # Debugging line
-            key, value = line.strip().split(': ')
-            jenis_senjata[int(key)] = value
-    return jenis_senjata
--- warnasenjata.py
+++
@@ -1,9 +0,0 @@
-# Fungsi untuk membaca file warna_senjata (data.txt)
-def baca_warna_senjata(filename):
-    warna_senjata = {}
-    with open(filename, 'r') as file:
-        for line in file:
-            print(f"Reading line: {line}")  # Debugging line
-            key, value = line.strip().split(': ')
-            warna_senjata[int(key)] = value
-    return warna_senjata
